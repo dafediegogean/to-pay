f8e367a7a7479e07f718aee645261e82334618e4
diegogeandafe <diegogeandafe@gmail.com>
2018-02-03 22:05:24 -0200

    Context path no Tomcat externo

--- src/main/java/com/algaworks/cobranca/CobrancaApplication.java
+++ src/main/java/com/algaworks/cobranca/CobrancaApplication.java
@@ -5,7 +5,10 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
@@ -19,4 +22,17 @@ public class CobrancaApplication {
 	public LocaleResolver localeResolver() {
 		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
+	
+	@Configuration
+	
+	//Configuração do redirect inicial, colocando localhost:8080/ redireciona diretamente
+	//para titulos
+	public static class MvcConfig extends WebMvcConfigurerAdapter {
+		
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/titulos");
+		}
+	}
+	
 }
